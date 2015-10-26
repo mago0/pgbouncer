ebdd5e399d544c3b042548b92d5e9f36e20dee81
Dorian Zaccaria <dorian@datadoghq.com>
2015-10-26 11:02:58 -0400

    [metadata] Bump the cookbook version

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'orichen@whitepages.com'
 license 'Apache 2.0'
 description 'Installs/Configures pgbouncer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.0.1'
 recipe 'pgbouncer', 'the default recipe does nothing to hopefully indicate that this is an LWRP cookbook'
 recipe 'pgbouncer::example', 'this gives an example of how one could consume the pgbouncer cookbook'
 
