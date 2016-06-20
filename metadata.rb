7821b208a32c2671c33cc19627b9b43ce6412638
Matt Williams <matt@tilt.com>
2016-06-20 15:34:48 -0400

    Increment version

--- metadata.rb
+++ metadata.rb
@@ -5,9 +5,9 @@ maintainer_email "orichen@whitepages.com"
 license          "Apache 2.0"
 description      "Installs/Configures pgbouncer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.1.0"
 recipe           "pgbouncer", "the default recipe does nothing to hopefully indicate that this is an LWRP cookbook"
-recipe		 "pgbouncer::example", "this gives an example of how one could consume the pgbouncer cookbook"
+recipe           "pgbouncer::example", "this gives an example of how one could consume the pgbouncer cookbook"
 
 %w{ubuntu}.each do |os|
   supports os
